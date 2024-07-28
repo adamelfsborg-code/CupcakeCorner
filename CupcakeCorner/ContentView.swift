aae93839593d692a0b22ff7ab1e9713d8a621815
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-28 08:49:09 +0200

    feat: validate and disable form

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -9,18 +9,23 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disabledFrom: Bool {
+        username.count < 5 || email.count < 5
+    }
+    
     var body: some View {
-        VStack {
-            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
-                if let image = phase.image {
-                    image.resizable().scaledToFit()
-                } else if phase.error != nil {
-                    Text("There was an error loading the image")
-                } else {
-                    ProgressView()
-                }
-            } 
-            .frame(width: 200, height: 200)
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            Section {
+                Button("Create account") { }
+            }
+            .disabled(disabledFrom)
         }
     }
     
